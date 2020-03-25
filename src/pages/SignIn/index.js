b3410772b7fc8be7a99731c2f115411096f73eb8
kielkow <matheuskiel@fiorifer.com.br>
2020-03-25 11:26:33 -0300

    Using Unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,25 +1,31 @@
+/* eslint-disable no-console */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 // import { Container } from './styles';
 import logo from '~/assets/logo.png';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="MarvelApp" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <label>E-MAIL</label>
-        <input name="email" type="email" placeholder="Your e-mail..." />
+        <Input name="email" type="email" placeholder="Your e-mail..." />
 
         <label>PASSWORD</label>
-        <input name="password" type="password" placeholder="Your password..." />
+        <Input name="password" type="password" placeholder="Your password..." />
 
         <button type="submit">Enter</button>
         <Link to="/register">Create free account</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,28 +1,34 @@
+/* eslint-disable no-console */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 // import { Container } from './styles';
 import logo from '~/assets/logo.png';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="MarvelApp" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <label>NAME</label>
-        <input name="name" placeholder="Your full name..." />
+        <Input name="name" placeholder="Your full name..." />
 
         <label>E-MAIL</label>
-        <input name="email" type="email" placeholder="Your e-mail..." />
+        <Input name="email" type="email" placeholder="Your e-mail..." />
 
         <label>PASSWORD</label>
-        <input name="password" type="password" placeholder="Your password..." />
+        <Input name="password" type="password" placeholder="Your password..." />
 
         <button type="submit">Create account</button>
         <Link to="/">I already have an account</Link>
-      </form>
+      </Form>
     </>
   );
 }
