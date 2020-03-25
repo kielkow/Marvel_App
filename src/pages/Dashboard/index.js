5b07d5571ccf709074d1318002c239bb33cf84c3
kielkow <matheuskiel@fiorifer.com.br>
2020-03-25 17:38:29 -0300

    Set token in all requisitions

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 // import { Container } from './styles';
+import api from '~/services/api';
 
 export default function Dashboard() {
+  api.get('appointments');
   return <div>Dashboard</div>;
 }
