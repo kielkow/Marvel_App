b34817377b4234226e721f514cf1cc6dcfeef6aa
kielkow <matheuskiel@fiorifer.com.br>
2020-03-26 20:35:56 -0300

    Starting hero page style

--- src/pages/Hero/index.js
+++ src/pages/Hero/index.js
@@ -1,8 +1,55 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { MdArrowBack, MdInfo } from 'react-icons/md';
+import { Input } from '@rocketseat/unform';
+
+import { Link } from 'react-router-dom';
+import { Container, Content } from './styles';
 
 export default function Hero() {
-  const heroedata = useSelector(state => state.hero);
-  console.log('HERO DATA', heroedata);
-  return <div>View Hero</div>;
+  const herodata = useSelector(state => state.hero);
+
+  return (
+    <Container>
+      <header>
+        <strong>
+          <MdInfo />
+          Hero Info
+        </strong>
+        <div>
+          <Link type="button" to="/dashboard">
+            <MdArrowBack color="#fff" size={18} />
+            <span>Back</span>
+          </Link>
+        </div>
+      </header>
+      <Content>
+        <img
+          src="https://api.adorable.io/avatars/50/abott@adorable.png"
+          alt={herodata.hero.name}
+        />
+        <div>
+          <div>
+            <span>NAME</span>
+            <Input
+              name="name"
+              disabled
+              value={herodata.hero.name || 'Any name detected'}
+            />
+          </div>
+          <div>
+            <span>DESCRIPTION</span>
+            <textarea
+              name="description"
+              disabled
+              value={
+                herodata.hero.description ||
+                'This hero don´t have a description'
+              }
+            />
+          </div>
+        </div>
+      </Content>
+    </Container>
+  );
 }
--- src/pages/Hero/styles.js
+++ src/pages/Hero/styles.js
@@ -1,3 +1,84 @@
 import styled from 'styled-components';
+import { lighten, darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 800px;
+  margin: 0px auto;
+  padding-top: 30px;
+  display: flex;
+  flex-direction: column;
+  header {
+    display: flex;
+    justify-content: space-between;
+    strong {
+      display: flex;
+      align-items: center;
+      font-size: 24px;
+      color: ${lighten(0.03, '#444444')};
+      svg {
+        margin-right: 5px;
+      }
+    }
+    div {
+      display: flex;
+      justify-content: center;
+      a {
+        background: #ee4d63;
+        color: #fff;
+        padding: 5px 10px;
+        border: 0;
+        font-weight: bold;
+        border-radius: 4px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        &:hover {
+          background: ${darken(0.05, '#ee4d63')};
+        }
+      }
+    }
+  }
+`;
+
+export const Content = styled.form`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  background: #fff;
+  margin-top: 30px;
+  border-radius: 4px;
+  padding: 20px;
+  img {
+    height: 160px;
+    width: 160px;
+    border-radius: 10%;
+    margin-right: 20px;
+  }
+  div {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    span {
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+    input {
+      padding: 10px 10px;
+      border-radius: 4px;
+      border: 0.5px solid #eee;
+    }
+    textarea {
+      font-size: 14px;
+      font-family: 'Roboto';
+      padding: 10px 10px;
+      border-radius: 4px;
+      border: 0.5px solid #eee;
+      flex: 1;
+      resize: none;
+    }
+    & + div {
+      margin-top: 20px;
+      height: 200px;
+    }
+  }
+`;
